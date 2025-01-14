e845317a06a23ae962fd5a1c61022508f68349f1
rodrigo <rodrigodsd@gmail.com>
2025-01-14 11:11:12 -0300

    LinkedList

---
+++ Java/dsa/linkedlists/InsertInMiddle.java
@@ -0,0 +1,116 @@
+package linkedlists;
+
+import java.io.IOException;
+import java.util.List;
+
+public class InsertInMiddle {
+    public static void main(String[] args) throws IOException {
+
+        List<Integer> nums = List.of(1, 2, 4, 5);
+
+        DoublyLinkedList llist = new DoublyLinkedList();
+
+        for (int i = 0; i < nums.size(); i++) {
+            llist.insertNode(nums.get(i));
+        }
+
+        llist.sortedInsert(llist.head, 3);
+
+        DoublyLinkedListPrintHelper.printList(llist.head);
+
+    }
+}
+
+
+class DoublyLinkedListNode {
+    public int data;
+    public DoublyLinkedListNode next;
+    public DoublyLinkedListNode prev;
+
+    public DoublyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    public DoublyLinkedListNode head;
+    public DoublyLinkedListNode tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
+
+        DoublyLinkedListNode head = llist;
+
+        while (llist.next != null) {
+
+            if (data > llist.data && data <= llist.next.data) {
+                DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+                DoublyLinkedListNode tmp = llist.next;
+                llist.next = newNode;
+                newNode.next = tmp;
+                newNode.prev = llist;
+            }
+
+            llist = llist.next;
+
+        }
+
+        return head;
+
+    }
+
+    public void insertNode(int nodeData) {
+        DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+    }
+}
+
+class DoublyLinkedListPrintHelper {
+    public static void printList(DoublyLinkedListNode root) throws IOException {
+        while (root != null) {
+            System.out.print(root.data + " -> ");
+            root = root.next;
+        }
+        System.out.print("\n");
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'sortedInsert' function below.
+     *
+     * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+     * The function accepts following parameters:
+     *  1. INTEGER_DOUBLY_LINKED_LIST llist
+     *  2. INTEGER data
+     */
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+
+
+}
+
